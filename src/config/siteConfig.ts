c2109afa1ebfcacb9e08f619c839e7a5272ad04e
aron <aronblake1010@gmail.com>
2026-07-06 15:32:58 +0800

    commit

--- src/config/siteConfig.ts
+++ src/config/siteConfig.ts
@@ -56,7 +56,7 @@ export const siteConfig: SiteConfig = {
 	favicon: [
 		{
 			// 图标文件路径
-			src: "/favicon/favicon.ico",
+			src: "/favicon/IMG_4428.jpg",
 			// 可选，指定主题 'light' | 'dark'
 			// theme: "light",
 			// 可选，图标大小
